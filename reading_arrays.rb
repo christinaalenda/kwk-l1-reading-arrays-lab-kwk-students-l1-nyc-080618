d266bc7a9448a8467455bc3849907a6ad832e68e
christinaalenda <clenda041@gmail.com>
2018-08-08 18:11:59 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -20,7 +20,7 @@ def last_student_by_index
 end
 
 def first_student_by_method
-  returns STUDENT_NAMES[String]
+  returns STUDENT_NAMES[string]
 
 def last_student_by_method
   returns STUDENT_NAMES[5]
